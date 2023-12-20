efa8abc72972f61164d0752b52b4a7b17818cb8b
Marcin Kowalski <108225313+mkowalski-zsme@users.noreply.github.com>
2023-12-20 14:00:07 +0100

    Create 2023.12.20 dynTabAvg.cpp

---
+++ 2023.12.20 dynTabAvg.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+double obliczSrednia(int** tablica, int liczbaWierszy, int liczbaKolumn) {
+    double suma;
+    double srednia;
+
+    for (int i = 0; i < liczbaWierszy; ++i) {
+        suma = 0.0;
+
+        for (int j = 0; j < liczbaKolumn; ++j) {
+            suma += tablica[i][j];
+        }
+
+        srednia = suma / liczbaKolumn;
+
+        std::cout << "Średnia dla wiersza " << i + 1 << " to: " << srednia << std::endl;
+    }
+}
+
+int main() {
+    const int liczbaWierszy = 3;
+    const int liczbaKolumn = 4;
+
+    // Alokuje dynamicznie dwuwymiarową tablicę
+    int** dynamicArray = new int*[liczbaWierszy];
+
+    for (int i = 0; i < liczbaWierszy; ++i) {
+        dynamicArray[i] = new int[liczbaKolumn];
+    }
+
+    // Inicjalizuje tablicę
+    for (int i = 0; i < liczbaWierszy; ++i) {
+        for (int j = 0; j < liczbaKolumn; ++j) {
+            dynamicArray[i][j] = i * liczbaKolumn + j;
+        }
+    }
+
+    // Oblicza i wypisuje średnie arytmetyczne
+    obliczSrednia(dynamicArray, liczbaWierszy, liczbaKolumn);
+
+    // Zwolnienie pamięci
+    for (int i = 0; i < liczbaWierszy; ++i) {
+        delete[] dynamicArray[i];
+    }
+    delete[] dynamicArray;
+
+    return 0;
+}
